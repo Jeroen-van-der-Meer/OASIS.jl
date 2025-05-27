41d7192639a7d7ffcb77355b3af71e115ca4d3c3
Jeroen van der Meer <meer@vivaldi.net>
2025-05-27 19:20:43 +0200

    Read reals

--- src/read.jl
+++ src/read.jl
@@ -1,24 +1,45 @@
-function read_unsigned_integer(buf; pos::Int = 1)
+function read_unsigned_integer(io::IO)
     output = 0
     shift = 0
     while true
-        b = get_byte(buf, pos)
+        b = read(io, UInt8)
         output += UInt64(b & 0x7F) << shift
         b & 0x80 == 0 && break
-        pos += 1
         shift += 7
     end
     return output
 end
 
-function read_signed_integer(buf; pos::Int = 1)
-    unsigned_output = read_unsigned_integer(buf; pos)
+function read_signed_integer(io::IO)
+    unsigned_output = read_unsigned_integer(io)
     sign = unsigned_output & one(UInt64)
     output = signed(unsigned_output >> 1)
     iszero(sign) ? output : -output
 end
 
-function get_byte(buf::AbstractVector{UInt8}, pos::Int)
-    @inbounds b = buf[pos]
-    return b
+read_positive_whole_number(io::IO) = signed(read_unsigned_integer(io))
+read_negative_whole_number(io::IO) = -signed(read_unsigned_integer(io))
+read_positive_reciprocal(io::IO) = 1 / read_unsigned_integer(io)
+read_negative_reciprocal(io::IO) = -1 / read_unsigned_integer(io)
+read_positive_ratio(io::IO) = read_positive_whole_number(io) / read_positive_whole_number(io)
+read_negative_ratio(io::IO) = read_negative_whole_number(io) / read_positive_whole_number(io)
+read_four_byte_float(io::IO) = Float64(read(io, Float32))
+read_eight_byte_float(io::IO) = read(io, Float64)
+
+const REAL_READER_PER_FORMAT = (
+    read_positive_whole_number,
+    read_negative_whole_number,
+    read_positive_reciprocal,
+    read_negative_reciprocal,
+    read_positive_ratio,
+    read_negative_ratio,
+    read_four_byte_float,
+    read_eight_byte_float
+)
+
+function read_real(io::IO)
+    real_format = read(io, UInt8) + 1
+    return REAL_READER_PER_FORMAT[real_format](io)
 end
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,33 @@
 using Oasis
 using Test
 
-@testset "Read integers" begin
+@testset "Read numbers" begin
     @testset "Read unsigned integers" begin
-        @test Oasis.read_unsigned_integer([0x00]) == 0
-        @test Oasis.read_unsigned_integer([0x7f]) == 127
-        @test Oasis.read_unsigned_integer([0x80, 0x01]) == 128
-        @test Oasis.read_unsigned_integer([0xff, 0x7f]) == 16383
-        @test Oasis.read_unsigned_integer([0x80, 0x80, 0x01]) == 16384
+        @test Oasis.read_unsigned_integer(IOBuffer([0x00])) == 0
+        @test Oasis.read_unsigned_integer(IOBuffer([0x7f])) == 127
+        @test Oasis.read_unsigned_integer(IOBuffer([0x80, 0x01])) == 128
+        @test Oasis.read_unsigned_integer(IOBuffer([0xff, 0x7f])) == 16383
+        @test Oasis.read_unsigned_integer(IOBuffer([0x80, 0x80, 0x01])) == 16384
     end
     @testset "Read signed integers" begin
-        @test Oasis.read_signed_integer([0x00]) == 0
-        @test Oasis.read_signed_integer([0x02]) == 1
-        @test Oasis.read_signed_integer([0x03]) == -1
-        @test Oasis.read_signed_integer([0x7e]) == 63
-        @test Oasis.read_signed_integer([0x81, 0x01]) == -64
-        @test Oasis.read_signed_integer([0xfe, 0x7f]) == 8191
-        @test Oasis.read_signed_integer([0x81, 0x80, 0x01]) == -8192
+        @test Oasis.read_signed_integer(IOBuffer([0x00])) == 0
+        @test Oasis.read_signed_integer(IOBuffer([0x02])) == 1
+        @test Oasis.read_signed_integer(IOBuffer([0x03])) == -1
+        @test Oasis.read_signed_integer(IOBuffer([0x7e])) == 63
+        @test Oasis.read_signed_integer(IOBuffer([0x81, 0x01])) == -64
+        @test Oasis.read_signed_integer(IOBuffer([0xfe, 0x7f])) == 8191
+        @test Oasis.read_signed_integer(IOBuffer([0x81, 0x80, 0x01])) == -8192
+    end
+    @testset "Read reals" begin
+        @test Oasis.read_real(IOBuffer([0x00, 0x00])) == 0.0
+        @test Oasis.read_real(IOBuffer([0x00, 0x01])) == 1.0
+        @test Oasis.read_real(IOBuffer([0x03, 0x02])) == -0.5
+        @test Oasis.read_real(IOBuffer([0x04, 0x05, 0x10])) == 0.3125
+        @test Oasis.read_real(IOBuffer([0x02, 0x03])) == 1/3
+        @test Oasis.read_real(IOBuffer([0x05, 0x02, 0x0d])) == -2/13
+        # In line below, LHS outputs Float64. Conversion to Float32 needed due to inherently
+        # limited precision.
+        @test Oasis.read_real(IOBuffer([0x06, 0xd9, 0x89, 0x1d, 0xbe])) == Float32(-2/13)
+        @test Oasis.read_real(IOBuffer([0x07, 0x14, 0x3b, 0xb1, 0x13, 0x3b, 0xb1, 0xc3, 0xbf])) == -2/13
     end
 end
