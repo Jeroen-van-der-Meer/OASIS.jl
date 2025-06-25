44a0c011cc2955c365b3bacb46bb1db480c5ec6c
Jeroen van der Meer <meer@vivaldi.net>
2025-06-26 00:51:16 +0200

    Attempt at plot extension

---
+++ ext/OasisPlots.jl
@@ -0,0 +1,29 @@
+module OasisPlots
+
+if isdefined(Base, :get_extension)
+    using OasisTools
+else
+    using ..OasisTools
+end
+
+using GeometryBasics
+using Makie
+
+function OasisTools.plot_shape!(ax::Axis, shape::Shape{<:Any})
+    @error "Not implemented"
+end
+
+function OasisTools.plot_shape!(ax::Axis, shape::Shape{<:AbstractGeometry{2, Int64}})
+    Makie.poly!(ax, shape.shape)
+end
+
+function OasisTools.plot_cell(cell::Cell)
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    for shape in cell.shapes
+        plot_shape!(ax, shape)
+    end
+    return fig
+end
+
+end # module OasisPlots
